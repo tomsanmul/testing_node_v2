adb57e9a1e47421a95e762960c42459e502ea372
tomsanmul <tomsan@gmail.com>
2022-12-20 22:50:34 +0100

    Prueba de Test en Initial_Test

--- Initial_Test/__test__/initial-test.test.js
+++ Initial_Test/__test__/initial-test.test.js
@@ -3,3 +3,19 @@ const sum = require('../initial-test/initial-test');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('If any parameter is empty, it fails.', () => {
+    expect(sum("", "")).toBe(false);
+});
+
+test('If we pass characters in any of the parameters, it fails', () => {
+    expect(sum("dog", "cat")).toBe(false);
+});
+
+test('If any parameter is negative, it fails.', () => {
+    expect(sum(-2, -5)).toBe(false);
+});
+
+
+
+
--- Initial_Test/initial-test/initial-test.js
+++ Initial_Test/initial-test/initial-test.js
@@ -1,4 +1,7 @@
 function sum(a, b) {
+    if ((isNaN(a)) || (isNaN(b))) return false;  //return false if any parameter is empty
+    if (!Number(a) || !Number(b)) return false;  //return false if any parameter is char/string
+    if ((a < 0) || (b < 0) ) return false;       //return false if any parameter is negative
     return a + b;
 }
 module.exports = sum;
