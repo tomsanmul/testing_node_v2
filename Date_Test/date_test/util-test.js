bf9b5f5b505f44c022e9ea6cecc2a6ff7134dedb
Tomas Sans Mulero <113888046+tomsanmul@users.noreply.github.com>
2022-12-21 13:17:20 +0100

    Update util-test.js

--- Date_Test/date_test/util-test.js
+++ Date_Test/date_test/util-test.js
@@ -3,26 +3,33 @@ const russianLocale = require('date-fns/locale/ru');
 const differenceInDays = require('date-fns/differenceInDays');
 const differenceInBusinessDays = require('date-fns/differenceInBusinessDays')
 
+
 const {
     orderdates,
-    substring,
-    formatDate1,
-    formatDate2,
-    formattedDateRu,
-    daysBetween,
-    workdaysBetween
-} = require('util.js');
-
-
-const dates = [
+   substring,
+   formatDate1,
+   formatDate2,
+   formattedDateRu,
+   daysBetween,
+   workdaysBetween
+  } = require('./__test__/utils.js');
+  
+  
+  
+  const dates = [
     new Date(1995, 6, 2),
     new Date(1987, 1, 11),
     new Date(1989, 6, 10),
   ]
-
-test('order the dates from smallest to largest', () => {
-    expect(orderdates(dates).toBe([new Date(1987, 1, 11),new Date(1989, 6, 10),new Date(1995, 6, 2) ]));
-});
-
   
 
+test('Test orderdates', () => {
+    expect(orderdates(dates)).toStrictEqual([new Date(1987, 1, 11), new Date(1989, 6, 10), new Date(1995, 6, 2)]);
+  });
+  
+  
+test('daysBetween', () => {
+    expect(daysBetween([new Date(1987, 1, 11), new Date(1987, 1, 12)])).toBe(1);
+  });
+  
+  
