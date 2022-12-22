49c7c3a1d148c9945c6b1eeb780bc7e86fcff281
Tomas Sans Mulero <113888046+tomsanmul@users.noreply.github.com>
2022-12-22 10:34:40 +0100

    Update dni.test.js

--- Initial_Test/__test__/dni.test.js
+++ Initial_Test/__test__/dni.test.js
@@ -1,7 +1,20 @@
 const comprovaDNI = require('../initial-test/dni');
 
-test('Comprova que retorni R aques DNI ', () => {
-  expect(comprovaDNI("38113164")).toBe('W');
+
+test('Comprovo que no li passi buit o undefined el DNI', () => {
+  expect(comprovaDNI("")).toBe(false);
 });
 
+
+test('Comprovo que la longitud del dni sigui de 8 numeros', () => {
+  expect(comprovaDNI("85758676584764")).toBe(false);
+  expect(comprovaDNI("257586")).toBe(false);
+});
+
+
+test('Comprova que retorni les lletres correctes a aquests DNIs ', () => {
+  expect(comprovaDNI("38113164")).toBe('W');
+  expect(comprovaDNI("37239594")).toBe('H');
+  expect(comprovaDNI("47718419")).toBe('C');
+});
 
