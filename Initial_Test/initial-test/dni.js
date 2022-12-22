3a7c274626a87b960e9466dc72ad3cbeaf35c281
Tomas Sans Mulero <113888046+tomsanmul@users.noreply.github.com>
2022-12-22 10:35:16 +0100

    Update dni.js

--- Initial_Test/initial-test/dni.js
+++ Initial_Test/initial-test/dni.js
@@ -2,12 +2,16 @@
 function ComprovaDNI(dni) {
 
     const letra = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
-    
+ 
+    if (dni == "") return(false);
     let numDNI = parseInt(dni);
-    calculo = numDNI % 23;
+    let strDNI = numDNI.toString();
+    if (strDNI.length != 8) return(false);
 
+    let calculo = numDNI % 23;
     return letra[calculo];
    
 }
 
+console.log(ComprovaDNI("38dsdr113164"));
 module.exports = ComprovaDNI;
