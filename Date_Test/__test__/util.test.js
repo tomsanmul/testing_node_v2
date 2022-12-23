945498914b2d5745682a36d5e09b70449a811f8e
tomsanmul <tomsan@gmail.com>
2022-12-23 23:43:48 +0100

    Tests acabats

--- Date_Test/__test__/util.test.js
+++ Date_Test/__test__/util.test.js
@@ -1,11 +1,6 @@
-const { compareAsc, format } = require( 'date-fns');
-const russianLocale = require('date-fns/locale/ru');
-const differenceInDays = require('date-fns/differenceInDays');
-const differenceInBusinessDays = require('date-fns/differenceInBusinessDays')
-
 
 const {
-    orderdates,
+   orderdates,
    substring,
    formatDate1,
    formatDate2,
@@ -22,14 +17,34 @@ const {
     new Date(1989, 6, 10),
   ]
   
-
-test('Test orderdates', () => {
+test('Test function in order to see if it organizes the array correctlyt', () => {
     expect(orderdates(dates)).toStrictEqual([new Date(1987, 1, 11), new Date(1989, 6, 10), new Date(1995, 6, 2)]);
   });
-  
-  
-test('daysBetween', () => {
-    expect(daysBetween(new Date(2022, 12, 22), new Date(2022, 12, 27))).toBe(5);
+
+
+const text = "Hello World";
+test('Test function in order to see if it extracts the characters between the two positions of the string correctly', () => {
+    expect(substring(text, 6, 11)).toBe("World");
   });
   
   
+const date1 = new Date("2022-12-22");
+test('Test function in order to see if it formats the date correctly', () => {
+    expect(formatDate1(date1)).toStrictEqual('12/22/2022');
+  });
+test('Test function in order to see if it formats the date correctly', () => {
+    expect(formatDate2(date1)).toStrictEqual('22-12-2022');
+  });
+test('Test function in order to see if it formats the date to a Russian date correctly', () => {
+    expect(formattedDateRu(date1)).toStrictEqual('декабря 22, 2022');
+  });
+
+
+test('Test function in order to see if it shows all the days between the two dates correctly', () => {
+    expect(daysBetween(new Date(2022, 12, 22), new Date(2022, 12, 27))).toBe(5);
+  });
+
+test('Test function in order to see if it shows all the workdays between the two dates correctly', () => {
+    expect(workdaysBetween(new Date(2022, 12, 22), new Date(2022, 12, 27))).toBe(4);
+  });
+
